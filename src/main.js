9f385ec1a7a416d415fb0b0cb61df12e531a62fb
Avtarso <t0676352927@gmail.com>
2024-12-17 22:13:15 +0200

    add work components

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// import './style.css'
+
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
+
+createApp(App).mount('#app');
+
